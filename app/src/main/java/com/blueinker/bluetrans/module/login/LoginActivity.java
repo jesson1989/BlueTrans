a069254fc4a2378d8c88e3111b1595de761fa2cb
jesson_yi <Jesson_Yi@asus.com>
2017-11-05 23:17:30 +0800

    2017.11.05 BlueTrans 第一次提交（登录页面界面显示部分完成）。

---
+++ app/src/main/java/com/blueinker/bluetrans/module/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.blueinker.bluetrans.module.login;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.blueinker.bluetrans.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
